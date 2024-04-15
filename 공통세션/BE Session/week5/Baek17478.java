7402754c784480feeead36185d187f785c74e676
dongha0312 <rsj1001@gmail.com>
2024-04-16 07:56:31 +0900

    백엔드 세션 3-1

---
+++ 공통세션/BE Session/week5/Baek17478.java
@@ -0,0 +1,29 @@
+package 공통세션.week5;
+
+import java.util.Scanner;
+
+public class Baek17478 {
+
+    static void
+    static void rec(int a){
+
+        System.out.println("\"재귀함수가 뭔가요?\"");
+        System.out.println("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
+        System.out.println("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
+        System.out.println("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+        return ;
+    }
+
+    //return "물었어" 밑에 재귀함수
+    //다음 라인 "-"4개
+    //라고 답변
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
+
+
+    }
+}
