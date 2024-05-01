fc074ff5ee74037fb7cced2d1ef84ba5698c3ac7
dongha0312 <rsj1001@gmail.com>
2024-05-01 14:25:30 +0900

    login assignment feedback

--- 공통세션/BE Session/Session4/SignUp.java
+++ 공통세션/BE Session/Session4/SignUp.java
@@ -8,20 +8,24 @@ import java.util.StringTokenizer;
 
 public class SignUp {
 
-//    1. 회원가입 선택 (y/n) (done)
-//    2. ID 입력 구현 (done)
-//    3. 비밀번호/비밀번호 확인 구현(done)
-//    4. 생년월일/ 생년월일 6자리 확인 구현(done)
-//    5. 이메일(done)
-//    6. login(done)
-//    7. 회원정보 불러오기(
-//    8. 회원정보 수정(
-//    9.종료 (done)
-//
+/*
+    1. 회원가입 선택 (y/n) 다른거 넣으면 끝남 (done)
+    2. ID 입력 구현 (done)
+    3. 비밀번호/비밀번호 확인 구현(done)
+    4. 생년월일/ 생년월일 6자리 확인 구현(done)
+    5. 이메일(done)
+    6. login(done)
+    7. 회원정보 불러오기(done)
+    8. 회원정보 수정(done)
+    9.종료 (done)
+
+    피드백 받은거 수정(done)
+    혹시 키 값들은 어떻게 받나요?
+*/
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        HashMap<String, String> id = new HashMap<>( );
-        HashMap<String, String> pw = new HashMap<>();
+        StringTokenizer st;
+        HashMap<String, String> Data = new HashMap<>();
         boolean success = false;
         String ID, PW;
 
@@ -32,7 +36,7 @@ public class SignUp {
             System.out.println("========================");
             String agree = br.readLine();
 
-            if(agree.equals("y") || agree.equals("Y")){
+            if(agree.equalsIgnoreCase("y")){
                 //아이디
                 System.out.println("=========================");
                 System.out.println("원하시는 아이디를 입력해주세요.");
@@ -44,36 +48,42 @@ public class SignUp {
                 System.out.println("비밀번호를 입력해주세요.");
                 System.out.println("=========================");
                 PW = br.readLine();
-                int count = 5;
-                for (int i = 0; i < 5; i++) {
+                int count = 6;
+                for (int i = 0; i < 6; i--) {
                     System.out.println("비밀번호를 확인합니다. 다시 한번 입력 해주세요.");
                     String re_PW = br.readLine();
 
                     if (!re_PW.equals(PW)) {
                         System.out.print("비밀번호가 틀렸습니다.");
-                        System.out.println("남은 횟수는 " + count + "입니다.");
+                        System.out.println("남은 횟수는 " + (count-1) + "입니다.");
                         count--;
-                        if (count == 0) {
-                            System.out.println("남은 횟수는 " + count + "입니다.");
-                            success = true;
-                            break;
-                        }
+
                     }
                     else if (re_PW.equals(PW)) {
                         System.out.println("비밀번호가 일치합니다.");
                         break;
                     }
+                    if (count == 0) {
+                        System.out.println("남은 횟수는 " + count + "입니다.");
+                        success = true;
+                        break;
+                    }
                 }
-                if (success == true) break;
+                if (success) break;
 
                 //생년월일 검사
                 System.out.println("=========================");
-                System.out.println("생년월일 6자리를 입력해주세요.");
+                System.out.println("생년월일 6자리 입력해주세요.");
                 System.out.println("=========================");
                 String birthDate = br.readLine();
+                if(birthDate.length() != 6){
+                    System.out.println("6자리가 아닙니다.");
+                    System.out.println("프로그램이 종료됩니다.");
+                    success = true;
+                }if (success) break;
 
-                int reBirthCnt = 5;
-                for (int i = 0; i < 5; i++) {
+                int reBirthCnt = 6;
+                for (int i = 0; i < 6; i++) {
                     System.out.println("생년월일 확인을 위해 한번 더 입력해주세요.");
                     String reBirthDate = br.readLine();
                     if (reBirthDate.equals(birthDate)){
@@ -81,14 +91,14 @@ public class SignUp {
                         break;
                     } else if (!reBirthDate.equals(birthDate)) {
                         System.out.println("생년월일이 틀렸습니다.");
-                        System.out.println("남은 횟수는 " + reBirthCnt + "입니다.");
+                        System.out.println("남은 횟수는 " + (reBirthCnt-1) + "입니다.");
                         reBirthCnt--;
                         if (reBirthCnt == 0) {
                             success = true;
                             break;
                         }
                     }
-                }if (success == true) break;
+                }if (success) break;
 
                 //이메일
                 System.out.println("=========================");
@@ -100,11 +110,12 @@ public class SignUp {
                 System.out.println("회원가입이 정상적으로 되었습니다.");
                 System.out.println("=========================");
 
-                id.put(ID,birthDate);
-                pw.put(PW,eMail);
-                System.out.println("ID: " + ID + " " + " Password: " + PW+ " Birthdate: " + birthDate + " Email: " +eMail );
+                Data.put(ID,birthDate);
+                Data.put(PW,eMail);
+                //혹시 키 값들은 어떻게 받나요?
+                System.out.println("ID: " + ID  + " Password: " + PW+ " Birthdate: " + Data.get(ID) + " Email: " +Data.get(PW) );
             }
-            else if (agree.equals("n") || agree.equals("N")){
+            else if (!agree.equalsIgnoreCase("y")){
                 System.out.println("회원가입을 종료합니다.");
                 break;
             }
@@ -120,42 +131,39 @@ public class SignUp {
                         System.out.println("=========================================");
                         System.out.println("로그인을 시작합니다. 아이디와 비밀번호를 입력해주세요.");
                         System.out.println("=========================================");
-                        StringTokenizer st = new StringTokenizer(br.readLine());
+                        st = new StringTokenizer(br.readLine());
                         String loginID = st.nextToken();
                         String loginPW = st.nextToken();
-                        id.get(loginID);
-                        pw.get(loginPW);
-
-                        if(id.containsKey(loginID) && pw.containsKey(loginPW)){
+                        if(Data.containsKey(loginID) && Data.containsKey(loginPW)){
                             System.out.println("로그인이 되었습니다.");
                         }
-                        if (!id.containsKey(loginID)){
+                        if (!Data.containsKey(loginID)){
                             System.out.println("없는 아이디 입니다.");
                         }
-                        else if (id.containsKey(loginID) && !pw.containsKey(loginPW)) {
+                        else if (Data.containsKey(loginID) && !Data.containsKey(loginPW)) {
                             System.out.println("비밀번호가 틀렸습니다.");
-                            int count = 5;
-                            for (int i = 0; i < 5; i++){
+                            int count = 6;
+                            for (int i = 0; i < 6; i++){
 
                                 System.out.println("======================");
                                 System.out.println("비밀번호를 다시 입력해주세요.");
                                 System.out.println("======================");
                                 String retryPW = br.readLine();
 
-                                if(!pw.containsKey(retryPW)){
+                                if(!Data.containsKey(retryPW)){
                                     System.out.print("비밀번호가 틀렸습니다.");
-                                    System.out.println("남은 횟수는 " + count + "입니다.");
+                                    System.out.println("남은 횟수는 " + (count-1) + "입니다.");
                                     count--;
                                     if (count == 0){
                                         System.out.println("남은 기회는 없습니다.");
                                         success = true;
                                     }
                                 }
-                                else if (pw.containsKey(retryPW)) {
+                                else if (Data.containsKey(retryPW)) {
                                     System.out.println("비밀번호가 맞았습니다. 로그인이 되었습니다.");
                                     break;
                                 }
-                            }if(success == true)break;
+                            }if(success)break;
                         }break;
 
                     //회원 정보 불러오기
@@ -166,18 +174,18 @@ public class SignUp {
                         String Id = st.nextToken();
                         String Pw = st.nextToken();
                         System.out.println("회원의 정보를 불러옵니다.");
-                        if (!id.containsKey(Id) || !pw.containsKey(Pw)){
+                        if (!Data.containsKey(Id) || !Data.containsKey(Pw)){
                             System.out.println("===========================");
                             System.out.println("입력하신 정보는 일치 하지 않습니다.");
                             System.out.println("===========================");
                         }
-                        if(id.containsKey(Id)&&pw.containsKey(Pw)){
+                        if(Data.containsKey(Id)&&Data.containsKey(Pw)){
                             System.out.println("================");
                             System.out.println("-----회원정보-----");
                             System.out.println("ID: " + Id);
                             System.out.println("PassWord: " + Pw);
-                            System.out.println("BirthDate: " + id.get(Id));
-                            System.out.println("Email: " + pw.get(Pw));
+                            System.out.println("BirthDate: " + Data.get(Id));
+                            System.out.println("Email: " + Data.get(Pw));
                         }
                         break;
 
@@ -189,7 +197,7 @@ public class SignUp {
                         st = new StringTokenizer(br.readLine());
                         String iD = st.nextToken();
                         String pW = st.nextToken();
-                        if (!id.containsKey(iD) || !pw.containsKey(pW)){
+                        if (!Data.containsKey(iD) || !Data.containsKey(pW)){
                             System.out.println("===========================");
                             System.out.println("입력하신 정보는 일치 하지 않습니다.");
                             System.out.println("===========================");
@@ -202,15 +210,15 @@ public class SignUp {
                             case 1:
                                 System.out.println("새로운 생년월일을 입력해주세요.");
                                 String newYear = br.readLine();
-                                id.replace(iD,id.get(iD),newYear);
-                                System.out.println("새로운 생년월일은 " + id.get(iD)+"입니다.");
+                                Data.replace(iD,Data.get(iD),newYear);
+                                System.out.println("새로운 생년월일은 " + Data.get(iD)+"입니다.");
                                 break;
 
                             case 2:
                                 System.out.println("새로운 이메일을 입력해주세요.");
                                 String newEmail = br.readLine();
-                                pw.replace(pW,pw.get(pW),newEmail);
-                                System.out.println("새로운 이메일은 " + pw.get(pW)+"입니다.");
+                                Data.replace(pW,Data.get(pW),newEmail);
+                                System.out.println("새로운 이메일은 " + Data.get(pW)+"입니다.");
                                 break;
                         }
 
